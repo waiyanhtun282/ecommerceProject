561d11d1c92b65a6627928ef8ddbb607d05f1165
waiyan282 <waiyanhtun282@gmail.com>
2023-12-18 23:05:51 +0630

    definemongose

--- util/database.js
+++ util/database.js
@@ -1,11 +1,27 @@
-const  Sequelize  = require('sequelize');
+const mongodb = require("mongodb");
+const MongodbClient = mongodb.MongoClient;
+// password=  kXrQoDlMhpP22ci5
+let _db;
+const mongoConnect = (callback) => {
+  MongodbClient.connect(
+    'mongodb+srv://waiyanhtun282:kXrQoDlMhpP22ci5@cluster0.hqcwcpc.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log("Connected");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
 
-
- const sequelize =new Sequelize('ecommerce-app','root','admin',{
-    dialect:'mysql',
-    host:'localhost'
- });
-
- module.exports =sequelize;
-
-
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No Database Found!";
+};
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
